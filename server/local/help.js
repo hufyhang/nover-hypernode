86bf7ef39165db23875c6eb1bd34c862c5c92d9c
Feifei HANG <l.daniel.hung@gmail.com>
2014-02-24 13:50:12 +0000

    updated help.js for cd command

--- server/local/help.js
+++ server/local/help.js
@@ -3,17 +3,21 @@
 var help = '';
 help += '--- General Purpose ---\n';
 help += 'cat \t-- concatenate files.\n';
+help += 'cd \t-- change directory.\n';
 help += 'ls \t-- list files.\n';
 help += 'exit \t-- exit HyperNode.\n';
+
 help += '\n--- File/Folder Management---\n';
 help += 'cp \t-- copy a file.\n';
 help += 'rm \t-- remove a file.\n';
 help += 'rm -r \t-- remove a folder.\n';
 help += 'mv \t-- move a file to somewhere.\n';
 help += 'mkdir \t-- create a folder.\n';
+
 help += '\n--- File Transfer ---\n';
 help += 'push \t-- upload a local file.\n';
 help += 'unzip \t-- unzip a zip file.\n';
+
 help += '\n--- Task Management ---\n';
 help += 'run \t-- run a Node.js file.\n';
 help += 'tasks \t-- list all running tasks.\n';
