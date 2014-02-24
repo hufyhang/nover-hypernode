081cd0a0e760f80639fe52b418da9c042a6e0390
Feifei HANG <l.daniel.hung@gmail.com>
2014-02-24 15:28:49 +0000

    Add -l flag for ls.js

--- server/local/ls.js
+++ server/local/ls.js
@@ -1,31 +1,65 @@
 #!/usr/bin/env node
 
 var fs   = require('fs');
-var argv = require('optimist').argv;
 var pa = require('path');
 
 var CWD = process.env.HYPERNODE_CWD;
 
-var path = pa.resolve(CWD, argv._[0] || '');
+var hasFlag = process.argv[2] === '-l';
+var targetDir;
+
+if (hasFlag) {
+  targetDir = process.argv[3] || '';
+} else {
+  targetDir = process.argv[2] || '';
+}
+
+var path = pa.resolve(CWD, targetDir);
 
 var real = fs.existsSync(path);
+
+var addItem = function (filename) {
+  'use strict';
+  var file = pa.basename(filename);
+  var fileStat = fs.statSync(filename);
+
+  if (fileStat.isDirectory()) {
+    return '[' + file + '] ';
+  } else {
+    return file + ' ';
+  }
+};
+
+var addItemDetail = function (filename) {
+  'use strict';
+  var file = pa.basename(filename);
+  var fileStat = fs.statSync(filename);
+  var buffer = '';
+
+  buffer += fileStat.size + '\t' + fileStat.mtime + '\t -- ';
+
+  if (fileStat.isDirectory()) {
+    buffer += '[' + file + ']\n';
+  } else {
+    buffer += file + '\n';
+  }
+  return buffer;
+};
 
 var buffer = '';
 if (real) {
   var stat = fs.statSync(path);
   if (stat.isDirectory()){
     var files = fs.readdirSync(path);
-    var fileStat;
     files.forEach(function (file) {
       'use strict';
       var filename = pa.resolve(path, file);
-      fileStat = fs.statSync(filename);
-
-      if (fileStat.isDirectory()) {
-        buffer += '[' + file + '] ';
+      if (hasFlag) {
+        buffer += addItemDetail(filename);
       } else {
-        buffer += file + ' ';
+        buffer += addItem(filename);
       }
+
     });
     console.log(buffer);
   } else {
