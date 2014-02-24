f2ea0009f36031bccc69ee63a10c3f12b627fd77
Feifei HANG <l.daniel.hung@gmail.com>
2014-02-24 17:13:56 +0000

    Added default 5 seconds timeout for now command

--- server/local/now.js
+++ server/local/now.js
@@ -4,12 +4,22 @@ var child = require('child_process');
 var pa = require('path');
 var CWD = process.env.HYPERNODE_CWD;
 var argv = process.argv;
+var DEFAULT_DURATION = 5 * 1000; // default process duration is 5 sec.
 
 if (argv.length < 3) {
-  console.error('Usage: now [filename] [args]');
+  console.error('Usage: now [duration] [filename] [args]');
   process.exit(0);
 }
+
+var dur;
 var cmd = argv.slice(2);
+
+if (/^[0-9]+$/.test(cmd[0])) {
+  dur = parseInt(cmd.shift(), 10) * 1000;
+} else {
+  dur = DEFAULT_DURATION;
+}
+
 cmd[0] = pa.resolve(CWD, cmd[0]);
 
 var task = child.spawn('node', cmd);
@@ -32,3 +42,8 @@ process.on('exit', function () {
   'use strict';
   task.kill();
 });
+
+setTimeout(function () {
+  'use strict';
+  process.exit(0);
+}, dur);
