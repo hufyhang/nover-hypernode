7b8049b5e010f59147cb2e964facd22aec766e4b
Feifei HANG <l.daniel.hung@gmail.com>
2014-02-25 16:41:46 +0000

    interpret file mode

--- server/local/ls.js
+++ server/local/ls.js
@@ -30,13 +30,60 @@ var addItem = function (filename) {
   }
 };
 
+var workOutModeCode = function (code) {
+  'use strict';
+  if (code === 7) {
+    return 'rwx';
+  }
+
+  if (code === 6) {
+    return 'rw-';
+  }
+
+  if (code === 5) {
+    return 'r-x';
+  }
+
+  if (code === 4) {
+    return 'r--';
+  }
+
+  if (code === 3) {
+    return '-wx';
+  }
+
+  if (code === 2) {
+    return '-w-';
+  }
+
+  if (code === 1) {
+    return '--w';
+  }
+
+  if (code === 0) {
+    return '---';
+  }
+};
+
+var interpretMode = function (code){
+  'use strict';
+  var user = Math.floor(code / 100);
+  var group = Math.floor((code - user * 100) / 10);
+  var others = Math.floor(code - user * 100 - group * 10);
+
+  return workOutModeCode(user) + workOutModeCode(group) + workOutModeCode(others);
+};
+
 var addItemDetail = function (filename) {
   'use strict';
   var file = pa.basename(filename);
   var fileStat = fs.statSync(filename);
   var buffer = '';
 
-  buffer += (fileStat.mode & parseInt('0777', 8)).toString(8) + '\t' +
+  var mode = (fileStat.mode & parseInt('0777', 8)).toString(8);
+  mode = parseInt(mode, 10);
+
+  buffer += interpretMode(mode) + '\t' +
     fileStat.size + '\t' + fileStat.mtime + '\t -- ';
 
   if (fileStat.isDirectory()) {
